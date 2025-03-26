59b40cfe48a1b1b073186e0de83a7e613641455d
jakmuell <jakob.mueller1004@gmail.com>
2025-03-27 00:41:57 +0100

    test

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='test2',
+    version='1.0',
+    packages=find_packages(),
+    package_data={'': ['py.typed']},
+    author='TennisProjekt',
+    description='Tennis Database',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/TennisProjekt/test2',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=['sqlalchemy', 'pandas', 'psycopg2']
+)
