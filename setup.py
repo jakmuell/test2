7f42bc0501e19c6180352addce9fc934b29239fb
jakmuell <jakob.mueller1004@gmail.com>
2025-03-27 20:38:29 +0100

    test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='test2',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     package_data={'': ['py.typed']},
     author='TennisProjekt',
